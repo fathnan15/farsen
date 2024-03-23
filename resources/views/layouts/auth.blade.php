d381f1e89a3a64c2561ce150038e5b5d8f0f1cec
fathnan15 <bnurito@gmail.com>
2024-03-23 17:01:47 +0700

    remove duplicates code

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,103 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-<!--begin::Head-->
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<!--begin::Head-->
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="{{ asset('app/images/logos/favicon.ico') }}" />
-    <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-    <!--end::Fonts-->
-    <!--begin::Global Stylesheets Bundle(used by all pages)-->
     <link href="{{ asset('metronic/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('metronic/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-    <!--end::Global Stylesheets Bundle-->
-
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="{{ asset('app/images/logos/favicon.ico') }}" />
-        <!--begin::Fonts-->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-        <!--end::Fonts-->
-        <!--begin::Global Stylesheets Bundle(used by all pages)-->
-        <link href="{{ asset('metronic/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('metronic/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <!--end::Global Stylesheets Bundle-->
-
-        <title>FARSEN @yield('title', '')</title>
-    </head>
-    <!--end::Head-->
-    <!--begin::Body-->
     <title>FARSEN @yield('title', '')</title>
 </head>
-<!--end::Head-->
-<!--begin::Body-->
 
 <body id="kt_body" class="bg-dark">
-    <!--begin::Main-->
     <div class="d-flex flex-column flex-root">
-        <!--begin::Authentication -->
         <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed"
-            style="background-image: url({{ asset('metronic/media/illustrations/sketchy-1/14-dark.png)') }}">
-            <!--begin::Content-->
+            style="background-image: url({{ asset('metronic/media/illustrations/sketchy-1/14-dark.png') }})">
             <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20" id="kt_content">
-                <!--begin::Logo-->
                 <img alt="Logo" src="{{ asset('app/images/logos/rsaugmwhite.svg') }}" class="h-70px mb-12" />
-                <!--end::Logo-->
-
                 @yield('content')
-
             </div>
-
-            <body id="kt_body" class="bg-dark">
-                <!--begin::Main-->
-                <div class="d-flex flex-column flex-root">
-                    <!--begin::Authentication -->
-                    <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed"
-                        style="background-image: url({{ asset('metronic/media/illustrations/sketchy-1/14-dark.png)') }}">
-                        <!--begin::Content-->
-                        <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20" id="kt_content">
-                            <!--begin::Logo-->
-                            <img alt="Logo" src="{{ asset('app/images/logos/rsaugmwhite.svg') }}"
-                                class="h-70px mb-12" />
-                            <!--end::Logo-->
-
-                            @yield('content')
-
-                        </div>
-                        <!--end::Content-->
-                    </div>
-                </div>
-                <!--end::Authentication-->
         </div>
-        <!--end::Main-->
-        <!--begin::Javascript-->
-        <script src="app/js/script.js"></script>
-        <!--begin::Global Javascript Bundle(used by all pages)-->
-        <script src="{{ asset('metronic/plugins/global/plugins.bundle.js') }}"></script>
-        <script src="{{ asset('metronic/js/scripts.bundle.js') }}"></script>
-        <!--end::Global Javascript Bundle-->
-        <!--begin::Page Custom Javascript(used by this page)-->
-        <script src="{{ asset('metronic/js/custom/authentication/sign-in/general.js') }}"></script>
-        <!--end::Page Custom Javascript-->
-        <!--end::Javascript-->
     </div>
-    <!--end::Main-->
-    <!--begin::Javascript-->
+
     <script src="app/js/script.js"></script>
-    <!--begin::Global Javascript Bundle(used by all pages)-->
     <script src="{{ asset('metronic/plugins/global/plugins.bundle.js') }}"></script>
     <script src="{{ asset('metronic/js/scripts.bundle.js') }}"></script>
-    <!--end::Global Javascript Bundle-->
-    <!--begin::Page Custom Javascript(used by this page)-->
     <script src="{{ asset('metronic/js/custom/authentication/sign-in/general.js') }}"></script>
-    <!--end::Page Custom Javascript-->
-    <!--end::Javascript-->
 
     @if ($errors->any())
         <script>
@@ -128,6 +56,5 @@
         </script>
     @endif
 </body>
-<!--end::Body-->
 
 </html>

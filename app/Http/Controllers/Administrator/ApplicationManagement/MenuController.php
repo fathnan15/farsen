e3365096776fbfca29d882393f71855e943e7302
fathnan15 <bnurito@gmail.com>
2024-08-05 23:49:41 +0700

    init menu management

--- app/Http/Controllers/Administrator/ApplicationManagement/MenuController.php
+++ app/Http/Controllers/Administrator/ApplicationManagement/MenuController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Administrator\ApplicationManagement;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserMenu;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
 class MenuController extends Controller
 {
-    //
+    public function menu(Request $request)
+    {
+        $menus = UserMenu::orderBy('menu_nm')->get();
+        return view('adm.menu.index', ['menus' => $menus]);
+        // dd('test menu controller');
+
+    }
 }
--- resources/views/adm/menu/index.blade.php
+++ resources/views/adm/menu/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.dashboard')
 @section('title-page', ' | ADMINISTRATION')
-@section('title-head', 'route')
+@section('title-head', 'menu')
 
 @section('content')
-    <h1>ROUTE MANAGEMENT</h1>
+    <h1>MENU MANAGEMENT</h1>
     <!--begin::Card-->
     <div class="card">
         <!--begin::Card header-->
@@ -14,8 +14,8 @@
                     <span class="svg-icon svg-icon-1 position-absolute ms-6">
                         <i class="fas fa-search fa-lg"></i>
                     </span>
-                    <input type="text" data-kt-route-table-filter="search"
-                        class="form-control form-control-solid w-250px ps-15" placeholder="Search Route" />
+                    <input type="text" data-kt-menu-table-filter="search"
+                        class="form-control form-control-solid w-250px ps-15" placeholder="Search Menu" />
                 </div>
             </div>
             <!--end::Search-->
@@ -23,8 +23,8 @@
             <!--begin::Card toolbar-->
             <div class="card-toolbar">
                 <!--begin::Toolbar-->
-                <div class="d-flex justify-content-end" data-kt-route-table-toolbar="base">
-                    <!--begin::Filter-->
+                <div class="d-flex justify-content-end" data-kt-menu-table-toolbar="base">
+                    {{-- <!--begin::Filter-->
                     <button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click"
                         data-kt-menu-placement="bottom-end">
                         <span class="svg-icon svg-icon-2">
@@ -42,7 +42,7 @@
                             <div class="mb-10">
                                 <label class="form-label fs-5 fw-bold mb-3">Request Method:</label>
                                 <div class="d-flex flex-column flex-wrap fw-bold"
-                                    data-kt-route-table-filter="request_method">
+                                    data-kt-menu-table-filter="request_method">
                                     <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
                                         <input class="form-check-input" type="radio" name="request_method" value="all"
                                             checked="checked" />
@@ -62,16 +62,16 @@
                             </div>
                             <div class="d-flex justify-content-end">
                                 <button type="reset" class="btn btn-light btn-active-light-primary me-2"
-                                    data-kt-menu-dismiss="true" data-kt-route-table-filter="reset">Reset</button>
+                                    data-kt-menu-dismiss="true" data-kt-menu-table-filter="reset">Reset</button>
                                 <button type="submit" class="btn btn-primary" data-kt-menu-dismiss="true"
-                                    data-kt-route-table-filter="filter">Apply</button>
+                                    data-kt-menu-table-filter="filter">Apply</button>
                             </div>
                         </div>
                     </div>
-                    <!--end::Filter-->
+                    <!--end::Filter--> --}}
                     <!--begin::Add Route-->
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" id="add_route"
-                        data-bs-target="#modal_add_route">Add Route</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" id="add_menu"
+                        data-bs-target="#modal_add_menu">Add Menu</button>
                     <!--end::Add Route-->
                 </div>
                 <!--end::Toolbar-->
@@ -82,67 +82,52 @@
 
         <!--begin::Card body-->
         <div class="card-body pt-0">
-            <table class="table align-middle table-row-dashed fs-6 gy-5" id="routes_table">
+            <table class="table align-middle table-row-dashed fs-6 gy-5" id="menus_table">
                 <thead>
                     <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
                         <th class="w-10px pe-2">
                             <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
                                 <input class="form-check-input" type="checkbox" data-kt-check="true"
-                                    data-kt-check-target="#routes_table .form-check-input" value="1" disabled hidden />
+                                    data-kt-check-target="#menus_table .form-check-input" value="1" disabled hidden />
                             </div>
                         </th>
-                        <th class="min-w-125px">Req:Route Name</th>
-                        <th class="min-w-125px">URI</th>
-                        <th class="min-w-125px">Controller:Method</th>
+                        <th class="min-w-125px">Menu Name</th>
+                        <th class="min-w-125px">Path</th>
+                        <th class="min-w-125px">Icon</th>
                         <th class="min-w-125px">Last Modified</th>
-                        <th class="min-w-50px">Middleware</th>
-                        <th class="min-w-50px">Status</th>
-                        <th class="text-end min-w-70px">Actions</th>
+                        <th class="text-center min-w-70px">Actions</th>
                     </tr>
                 </thead>
                 <tbody class="fw-bold text-gray-600">
-                    @foreach ($routes as $route)
-                        @php
-                            if ($route->is_active == 0 || Str::contains($route->uri, '')) {
-                                $hrefUri = '#';
-                            } else {
-                                $hrefUri = route($route->name);
-                            }
-                        @endphp
-                        <tr id="route_{{ $route->id }}">
+                    @foreach ($menus as $menu)
+                        <tr id="menu_{{ $menu->id }}">
                             <td>
                                 <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                    <input class="form-check-input" type="checkbox" value="{{ $route->id }}" disabled
+                                    <input class="form-check-input" type="checkbox" value="{{ $menu->id }}" disabled
                                         hidden />
                                 </div>
                             </td>
-                            <td data-filter = "{{ $route->http_req }}">
-                                <a href="{{ $hrefUri }}" class="text-gray-800 text-hover-primary mb-1">
-                                    {{ $route->name }}
-                                    <br>
-                                    : <span
-                                        class="{{ $route->http_req == 'post' ? 'text-success' : 'text-primary' }}">{{ $route->http_req }}</span>
-                                </a>
+                            <td data-filter = "{{ $menu->menu_nm }}">
+                                <span class="text-gray-800 mb-1">
+                                    {{ $menu->menu_nm }}
+                                </span>
                             </td>
                             <td>
-                                <a href="{{ $hrefUri }}"
-                                    class="text-gray-600 text-hover-primary mb-1">{{ $route->uri }}</a>
+                                <span class="text-gray-600 mb-1">{{ $menu->path }}</span>
                             </td>
                             <td>
-                                {{ $route->controller }}
-                                <br>
-                                : {{ $route->action }}
+                                <span class="text-gray-600 mb-1">{{ $menu->icon }}</span>
                             </td>
-                            <td>{{ $route->updated_at }}</td>
-                            <td>
+                            <td>{{ $menu->updated_at }}</td>
+                            {{-- <td>
                                 <div class="col-xl-9">
                                     <div class="form-check form-switch form-check-custom form-check-solid">
                                         <input class="middleware-checkbox form-check-input" type="checkbox"
-                                            value="{{ $route->id }}" id="routeStatus_{{ $route->id }}"
-                                            name="is_auth" {{ $route->is_auth == 1 ? 'checked' : '' }}>
+                                            value="{{ $menu->id }}" id="menuStatus_{{ $menu->id }}" name="is_auth"
+                                            {{ $menu->is_auth == 1 ? 'checked' : '' }}>
                                         <label class="form-check-label fw-bold text-gray-400 ms-3"
-                                            for="routeStatus_{{ $route->id }}">
-                                            @switch($route->is_auth)
+                                            for="menuStatus_{{ $menu->id }}">
+                                            @switch($menu->is_auth)
                                                 @case(1)
                                                     Auth
                                                 @break
@@ -161,11 +146,11 @@
                                 <div class="col-xl-9">
                                     <div class="form-check form-switch form-check-custom form-check-solid">
                                         <input class="status-checkbox form-check-input" type="checkbox"
-                                            value="{{ $route->id }}" id="routeStatus_{{ $route->id }}"
-                                            name="is_active" {{ $route->is_active == 1 ? 'checked' : '' }}>
+                                            value="{{ $menu->id }}" id="menuStatus_{{ $menu->id }}"
+                                            name="is_active" {{ $menu->is_active == 1 ? 'checked' : '' }}>
                                         <label class="form-check-label fw-bold text-gray-400 ms-3"
-                                            for="routeStatus_{{ $route->id }}">
-                                            @switch($route->is_active)
+                                            for="menuStatus_{{ $menu->id }}">
+                                            @switch($menu->is_active)
                                                 @case(1)
                                                     Active
                                                 @break
@@ -179,15 +164,14 @@
                                         </label>
                                     </div>
                                 </div>
-                            </td>
-                            <td class="text-end">
-                                <a href="#" class="edit-route-button btn btn-sm btn-light btn-active-light-primary"
-                                    data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end"
-                                    data-bs-toggle="modal" id="edit_route" data-id="{{ $route->id }}"
-                                    data-name="{{ $route->name }}" data-http_req="{{ $route->http_req }}"
-                                    data-uri="{{ $route->uri }}" data-controller="{{ $route->controller }}"
-                                    data-action="{{ $route->action }}" data-url ="{{ Route('app.route.update') }}"
-                                    data-bs-target="#editRouteModal">Edit
+                            </td> --}}
+                            <td class="text-center">
+                                <a href="#" class="edit-menu-button btn btn-sm btn-light btn-active-light-primary"
+                                    data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-bs-toggle="modal"
+                                    id="edit_menu" data-id="{{ $menu->id }}" data-name="{{ $menu->name }}"
+                                    data-http_req="{{ $menu->http_req }}" data-uri="{{ $menu->uri }}"
+                                    data-controller="{{ $menu->controller }}" data-action="{{ $menu->action }}"
+                                    data-url ="" data-bs-target="#editRouteModal">Edit
 
                                     <span class="svg-icon svg-icon-5 m-0">
                                         <i class="fas fa-edit"></i>
@@ -204,10 +188,10 @@
     <!--end::Card-->
 
     <!--begin::Modal Add Route-->
-    @include('adm.route.modal.add_route')
+    {{-- @include('adm.menu.modal.add_menu') --}}
     <!--end::Add Route-->
     <!--begin::Modal Edit Route-->
-    @include('adm.route.modal.edit_route')
+    {{-- @include('adm.menu.modal.edit_menu') --}}
     <!--end::Edit Route-->
 @endsection
 
@@ -217,9 +201,9 @@
     <!--end::Metronic JS-->
 
     <!--begin::App JS -->
-    <script src="{{ asset('app/js/adm/route/route.js') }}"></script>
-    <script src="{{ asset('app/js/adm/route/edit_route.js') }}"></script>
-    <script src="{{ asset('app/js/adm/route/add_route.js') }}"></script>
+    <script src="{{ asset('app/js/adm/menu/menu.js') }}"></script>
+    <script src="{{ asset('app/js/adm/menu/edit_menu.js') }}"></script>
+    <script src="{{ asset('app/js/adm/menu/add_menu.js') }}"></script>
     <!--end::App JS -->
 @endsection
 
@@ -227,7 +211,7 @@
     @if ($errors->any())
         <script>
             document.addEventListener("DOMContentLoaded", function() {
-                document.getElementById("add_route").click();
+                document.getElementById("add_menu").click();
             });
         </script>
     @endif

95cdf09257edfe038fcab1bdef4e89aac05b0b4c
fathnan15 <bnurito@gmail.com>
2024-08-14 00:24:20 +0700

    get menu detail by async

--- public/app/js/adm/menu/menu.js
+++ public/app/js/adm/menu/menu.js
@@ -58,84 +58,20 @@ var RoutesList = (function () {
                         "fa-minus-square"
                     );
 
-                    let submenu = `
+                    let loadingRow = `
                         <tr class='submenu menu-row' id=submenu_${id}>
                             <td colspan=6>
-                                <div class="flex-lg-row-fluid ms-10">
-                                    <ul class="nav nav-tabs border-0 fs-5 fw-bold">
-                                        <li class="nav-item">
-                                            <a class="nav-link text-active-primary pb-2 active" data-bs-toggle="tab" href="#kt_menu_view_overview_tab">Overview</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link text-active-primary pb-2" data-bs-toggle="tab" href="#kt_menu_view_submenu_tab">Submenu</a>
-                                        </li>
-                                    </ul>
-                                    <div class="tab-content rounded border-primary border border-dashed" id="tabContentMenu_${id}">
-                                        <div class="tab-pane fade show active" id="kt_menu_view_overview_tab" role="tabpanel">
-                                            <div class="card">
-                                                <div id="kt_menu_view" class="card-body px-5 px-5 bg-light-primary">
-                                                    <div data-kt-menu="row">
-                                                        <div id="kt_menu_view_1" data-bs-parent="#kt_menu_view">
-                                                            <div class="d-flex px-4" style="justify-content: flex-end">
-                                                                <span data-bs-toggle="tooltip" data-bs-trigger="hover" title="" data-bs-original-title="Edit menu details">
-                                                                    <a href="#" class="btn btn-sm btn-light-warning" data-bs-toggle="modal" data-bs-target="#kt_modal_update_menu">Edit</a>
-                                                                </span>
-                                                            </div>
-                                                            <div class="d-flex flex-wrap">
-                                                                <table class="table table-flush fw-bold gy-1">
-                                                                    <tbody>
-                                                                        <tr>
-                                                                            <th class="text-muted min-w-125px w-125px">Menu Name</th>
-                                                                            <td class="text-gray-800">
-                                                                                <div class="me-3">
-                                                                                    <div class="d-flex align-items-center">
-                                                                                        <div class="text-gray-800 fw-bolder">
-                                                                                            ${id}
-                                                                                        </div>
-                                                                                        <div class="badge badge-light-success ms-5">
-                                                                                            <a href="#" class="btn-active-light-primary w-30px h-30px" data-bs-toggle="tooltip" title="" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start">
-                                                                                                <span class="svg-icon svg-icon-3">
-                                                                                                    Active
-                                                                                                </span>
-                                                                                            </a>
-                                                                                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold w-150px py-3" data-kt-menu="true">
-                                                                                                <div class="menu-item px-3">
-                                                                                                    <a href="#" class="menu-link px-3" data-kt-payment-mehtod-action="set_as_deactive">Deactive</a>
-                                                                                                </div>
-                                                                                            </div>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <th class="text-muted min-w-125px w-125px">path</th>
-                                                                            <td class="text-gray-800">${id}</td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <th class="text-muted min-w-125px w-125px">Icon</th>
-                                                                            <td class="text-gray-800">
-                                                                                <span>
-                                                                                    <i class="${id}">
-                                                                                    </i>
-                                                                                </span>
-                                                                                ${id}
-                                                                            </td>
-                                                                        </tr>
-                                                                    </tbody>
-                                                                </table>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                <div class="text-center">
+                                    <div class="spinner-border text-primary" role="status">
+                                        <span class="visually-hidden">Loading...</span>
                                     </div>
                                 </div>
                             </td>
                         </tr>
                         `;
-                    menuRow.insertAdjacentHTML("afterend", submenu);
+                    menuRow.insertAdjacentHTML("afterend", loadingRow);
+
+                    fetchMenuDetails(id);
                 } else {
                     this.classList.remove(
                         "expand-detail-btn",
@@ -169,6 +105,136 @@ var RoutesList = (function () {
                 });
             });
         });
+    };
+
+    const fetchMenuDetails = (id) => {
+        fetch(`/adm/app/menu/${id}/details`)
+            .then((response) => response.json())
+            .then((data) => {
+                updateMenuDetails(id, data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                showErrorMessage(id);
+            });
+    };
+
+    const updateMenuDetails = (id, data) => {
+        const submenuRow = document.getElementById(`submenu_${id}`);
+        console.table(data);
+        console.log(data.sub_menu);
+        if (submenuRow) {
+            let ren = `
+            <td colspan=6>
+                <div class="flex-lg-row-fluid ms-10">
+                    <ul class="nav nav-tabs border-0 fs-5 fw-bold">
+                        <li class="nav-item">
+                            <a class="nav-link text-active-primary pb-2 active" data-bs-toggle="tab" href="#kt_menu_view_overview_tab">Overview</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-active-primary pb-2" data-bs-toggle="tab" href="#kt_menu_view_submenu_tab">Submenu</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content rounded border-primary border border-dashed" id="tabContentMenu_${id}">
+                        <div class="tab-pane fade show active" id="kt_menu_view_overview_tab" role="tabpanel">
+                            <div class="card">
+                                <div id="kt_menu_view" class="card-body px-5 px-5 bg-light-primary">
+                                    <div data-kt-menu="row">
+                                        <div id="kt_menu_view_1" data-bs-parent="#kt_menu_view">
+                                            <div class="d-flex px-4" style="justify-content: flex-end">
+                                                <span data-bs-toggle="tooltip" data-bs-trigger="hover" title="Edit menu details" data-bs-original-title="Edit menu details">
+                                                    <a href="#" class="btn btn-sm btn-light-warning" data-bs-toggle="modal" data-bs-target="#kt_modal_update_menu">Edit</a>
+                                                </span>
+                                            </div>
+                                            <div class="d-flex flex-wrap">
+                                                <table class="table table-flush fw-bold gy-1">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th class="text-muted min-w-125px w-125px">Menu Name</th>
+                                                            <td class="text-gray-800">
+                                                                <div class="me-3">
+                                                                    <div class="d-flex align-items-center">
+                                                                        <div class="text-gray-800 fw-bolder">
+                                                                            ${
+                                                                                data.menu_nm
+                                                                            }
+                                                                        </div>
+                                                                        <div class="badge badge-light-${
+                                                                            data.is_active ===
+                                                                            0
+                                                                                ? `danger`
+                                                                                : `success`
+                                                                        } ms-5">
+                                                                            <a href="#" class="btn-active-light-primary ${
+                                                                                data.is_active ===
+                                                                                0
+                                                                                    ? `text-danger`
+                                                                                    : ``
+                                                                            } w-30px h-30px" data-bs-toggle="tooltip" data-id=${
+                data.id
+            } title="click to ${
+                data.is_active === 0 ? `set active` : `set inactive`
+            }" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start">
+                                                                                <span class="svg-icon svg-icon-3">
+                                                                                    ${
+                                                                                        data.is_active ===
+                                                                                        0
+                                                                                            ? `inactive`
+                                                                                            : `active`
+                                                                                    }
+                                                                                </span>
+                                                                            </a>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th class="text-muted min-w-125px w-125px">path</th>
+                                                            <td class="text-gray-800">${
+                                                                data.path
+                                                            }</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th class="text-muted min-w-125px w-125px">Icon</th>
+                                                            <td class="text-gray-800">
+                                                                <span>
+                                                                    <i class="${
+                                                                        data.icon
+                                                                    }">
+                                                                    </i>
+                                                                </span>
+                                                                |
+                                                                ${data.icon}
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        `;
+            submenuRow.innerHTML = ren;
+        }
+    };
+
+    const showErrorMessage = (id) => {
+        const submenuRow = document.getElementById(`submenu_${id}`);
+        if (submenuRow) {
+            submenuRow.innerHTML = `
+                <td colspan=6>
+                    <div class="alert alert-danger" role="alert">
+                        An error occurred while fetching menu details. Please try again later.
+                    </div>
+                </td>
+            `;
+        }
     };
 
     return {
